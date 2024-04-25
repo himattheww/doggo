aabba540c73bf4db485cfe8d2a9d5ec26e5300a0
himattheww <matthewionwyn07@gmail.com>
2024-04-25 12:38:55 +0700

    "Header Styling"

---
+++ src/Components/Headerstyle.jsx
@@ -0,0 +1,23 @@
+const header = {
+    fontFamily: "Roboto, sans-serif",
+  backgroundColor: "black",
+};
+
+const typhographyx = {
+  fontFamily: "monospace",
+  flexgrow: 1,
+  color: "white",
+};
+
+const buttonn = {
+  color: "white",
+  "&:hover": {
+    backgroundColor: "#1565c0",
+  },
+};
+
+export const headerStyle = {
+  header,
+  typhographyx,
+  buttonn,
+};
---
+++ src/Pages/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../Components/Header";
+
+const Homepage = () => {
+  return (
+    <>
+      <Header />
+      <h1>Homepage</h1>
+    </>
+  );
+};
+
+export default Homepage;
